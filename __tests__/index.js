f07fd897023e7dd5e53917b8bc7402b360e755a0
Jeff Escalante <gh.je@mailhero.io>
2019-07-17 22:01:57 -0400

    fix tests

--- __tests__/index.js
+++ __tests__/index.js
@@ -1,8 +1,8 @@
-const fs = require("fs-extra");
-const path = require("path");
-const webpack = require("webpack");
-const merge = require("webpack-merge");
-const { PrebuildWebpackPlugin } = require("../index.js");
+const fs = require('fs-extra')
+const path = require('path')
+const webpack = require('webpack')
+const merge = require('webpack-merge')
+const PrebuildWebpackPlugin = require('../index.js')
 
 const getConfig = type => {
   return {
@@ -10,130 +10,132 @@ const getConfig = type => {
     output: {
       path: path.resolve(__dirname, `fixtures/${type}/dist`)
     }
-  };
-};
+  }
+}
 
-describe("PrebuildWebpackPlugin", () => {
-  describe("matched files", () => {
-    it("returns empty array if no matcher provided", done => {
-      const config = merge(getConfig("basic"), {
+describe('PrebuildWebpackPlugin', () => {
+  describe('matched files', () => {
+    it('returns empty array if no matcher provided', done => {
+      const config = merge(getConfig('basic'), {
         plugins: [
           new PrebuildWebpackPlugin({
             build: () => {}
           })
         ]
-      });
+      })
 
-      const compiler = webpack(config);
+      const compiler = webpack(config)
       compiler.run(async (err, stats) => {
+        if (err) return done(err)
         const matchedFiles = await stats.compilation.options.plugins[0]
-          .matchedFiles;
-        expect(matchedFiles).toStrictEqual([]);
-        done();
-      });
-    });
+          .matchedFilesCache
+        expect(matchedFiles).toStrictEqual(undefined)
+        done()
+      })
+    })
 
-    it("returns matched files given a proper `files.pattern` matcher", done => {
-      const config = merge(getConfig("files"), {
+    it('returns matched files given a proper `files.pattern` matcher', done => {
+      const config = merge(getConfig('files'), {
         plugins: [
           new PrebuildWebpackPlugin({
             files: {
-              pattern: "**/*.json",
-              options: { cwd: getConfig("files").context }
+              pattern: '**/*.json',
+              options: { cwd: getConfig('files').context }
             },
             build: () => {}
           })
         ]
-      });
+      })
 
-      const compiler = webpack(config);
+      const compiler = webpack(config)
       compiler.run(async (err, stats) => {
+        if (err) return done(err)
         const matchedFiles = await stats.compilation.options.plugins[0]
-          .matchedFiles;
-        expect(matchedFiles.length).toBe(3);
-        done();
-      });
-    });
-  });
+          .matchedFilesCache
+        expect(matchedFiles.length).toBe(3)
+        done()
+      })
+    })
+  })
 
-  describe("build", () => {
-    const outputDir = path.resolve(getConfig("basic").context, ".output");
-    const filePath = path.join(outputDir, "io.txt");
-    const fileMsg = "I/O test...";
+  describe('build', () => {
+    const outputDir = path.resolve(getConfig('basic').context, '.output')
+    const filePath = path.join(outputDir, 'io.txt')
+    const fileMsg = 'I/O test...'
 
-    beforeEach(() => fs.ensureDirSync(outputDir));
+    beforeEach(() => fs.ensureDirSync(outputDir))
 
     afterEach(() => {
       if (fs.existsSync(filePath)) {
-        fs.unlinkSync(filePath);
+        fs.unlinkSync(filePath)
       }
-    });
+    })
 
-    it("throws when not provided a build argument", () => {
+    it('throws when not provided a build argument', () => {
       expect(() => {
-        merge(getConfig("basic"), {
+        merge(getConfig('basic'), {
           plugins: [new PrebuildWebpackPlugin({})]
-        });
-      }).toThrow();
-    });
+        })
+      }).toThrow()
+    })
 
-    it("properly performs I/O", done => {
-      const config = merge(getConfig("basic"), {
+    it('properly performs I/O', done => {
+      const config = merge(getConfig('basic'), {
         plugins: [
           new PrebuildWebpackPlugin({
-            build: (compiler, compilation, matchedFiles) => {
-              fs.writeFile(filePath, new Uint8Array(Buffer.from(fileMsg)));
+            build: () => {
+              fs.writeFile(filePath, new Uint8Array(Buffer.from(fileMsg)))
             }
           })
         ]
-      });
+      })
 
-      const compiler = webpack(config);
+      const compiler = webpack(config)
       compiler.run(() => {
-        expect(fs.existsSync(filePath)).toBe(true);
-        expect(fs.readFileSync(filePath, { encoding: "utf8" })).toBe(fileMsg);
-        done();
-      });
-    });
+        expect(fs.existsSync(filePath)).toBe(true)
+        expect(fs.readFileSync(filePath, { encoding: 'utf8' })).toBe(fileMsg)
+        done()
+      })
+    })
 
-    it("works when provided a synchronous callback function", done => {
-      const config = merge(getConfig("basic"), {
+    it('works when provided a synchronous callback function', done => {
+      const config = merge(getConfig('basic'), {
         plugins: [
           new PrebuildWebpackPlugin({
-            build: (compiler, compilation, matchedFiles) => {
-              fs.writeFileSync(filePath, new Uint8Array(Buffer.from(fileMsg)));
+            build: () => {
+              fs.writeFileSync(filePath, new Uint8Array(Buffer.from(fileMsg)))
             }
           })
         ]
-      });
+      })
 
-      const compiler = webpack(config);
+      const compiler = webpack(config)
       compiler.run(() => {
-        expect(fs.existsSync(filePath)).toBe(true);
-        expect(fs.readFileSync(filePath, { encoding: "utf8" })).toBe(fileMsg);
-        done();
-      });
-    });
-  });
+        expect(fs.existsSync(filePath)).toBe(true)
+        expect(fs.readFileSync(filePath, { encoding: 'utf8' })).toBe(fileMsg)
+        done()
+      })
+    })
+  })
 
-  describe("watch", () => {
-    jest.setTimeout(10000);
+  describe('watch', () => {
+    jest.setTimeout(10000)
 
-    xit("provides a changed file", done => {
-      const config = merge(getConfig("files"), {
-        mode: "development",
+    xit('provides a changed file', done => {
+      const config = merge(getConfig('files'), {
+        mode: 'development',
         plugins: [
           new PrebuildWebpackPlugin({
             files: {
-              pattern: "**/*.json",
-              options: { cwd: getConfig("files").context, realpath: true },
+              pattern: '**/*.json',
+              options: { cwd: getConfig('files').context, realpath: true },
               addFilesAsDependencies: true
             },
             build: () => {},
             watch: (compiler, compilation, changedFile) => {
-              console.log(changedFile);
-              expect(changedFile.length).toBe(1);
-              done();
+              console.log(changedFile)
+              expect(changedFile.length).toBe(1)
+              done()
             }
           })
         ],
@@ -141,22 +143,22 @@ describe("PrebuildWebpackPlugin", () => {
         watchOptions: {
           poll: 300
         }
-      });
-      const compiler = webpack(config);
-      const watching = compiler.watch({}, (err, stats) => {});
+      })
+      const compiler = webpack(config)
+      const watching = compiler.watch({}, (err, stats) => {})
 
       setTimeout(() => {
         fs.writeFileSync(
-          path.resolve(getConfig("files").context, "one.json"),
+          path.resolve(getConfig('files').context, 'one.json'),
           '{"one" : "one"}'
-        );
-      }, 3000);
+        )
+      }, 3000)
 
       setTimeout(() => {
         watching.close(() => {
-          done();
-        });
-      }, 8000);
-    });
-  });
-});
+          done()
+        })
+      }, 8000)
+    })
+  })
+})
--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const glob = promisify(require('glob'))
 const minimatch = require('minimatch')
 const debug = require('debug')('prebuild-webpack-plugin')
 
+const PLUGIN_NAME = 'PrebuildWebpackPlugin'
+
 module.exports = class PrebuildWebpackPlugin {
   constructor({
     build,
@@ -25,9 +27,11 @@ module.exports = class PrebuildWebpackPlugin {
     this.clearCacheOnUpdate = clearCacheOnUpdate
   }
 
+  // If the user provided a `files: { pattern }` config, this function will
+  // go and fetch all the matching files from the filesystem.
   async getMatchedFiles() {
-    if (this.matchedFilesCache) return this.matchedFilesCache
     if (!this.files.pattern) return []
+    if (this.matchedFilesCache) return this.matchedFilesCache
 
     debug('start: get matched files')
     const options = this.files.options
@@ -40,6 +44,8 @@ module.exports = class PrebuildWebpackPlugin {
     return files
   }
 
+  // Getting the file that changes per watch trigger is a bit involved when it comes
+  // to getting it out of the webpack compilation object - this abstracts out the process.
   getChangedFile(compiler) {
     const { watchFileSystem } = compiler
     const watcher = watchFileSystem.watcher || watchFileSystem.wfs.watcher
@@ -47,11 +53,8 @@ module.exports = class PrebuildWebpackPlugin {
     return changedFile
   }
 
-  // This option was introduced for efficient use within nextjs
-  // As documented here: https://nextjs.org/docs#customizing-webpack-config,
-  // nextjs executes webpack twice, once for the server, and once for the client
-  // To prevent doing work 2x, we will take the passed compilation name and only run
-  // the logic for that specific compilation. For nextjs, you want "client"
+  // If a compilation name is given, returns whether the current compilation matches
+  // If not, returns false
   filterCompilation(compilation) {
     if (!compilation.name || !this.compilationNameFilter) return false
     return compilation.name !== this.compilationNameFilter
@@ -64,79 +67,71 @@ module.exports = class PrebuildWebpackPlugin {
     return this.build(compiler, compilation, matchedFiles)
   }
 
+  // "apply" is the main entrypoint for webpack plugins
   apply(compiler) {
     // The "beforeRun" hook runs only on single webpack build, triggering only once
-    compiler.hooks.beforeRun.tapPromise(
-      'PrebuildWebpackPlugin',
-      async compilation => {
-        if (this.filterCompilation(compilation)) return Promise.resolve()
-        debug(`running "beforeRun" hook for compilation: ${compilation.name}`)
-        this.runInitialBuild(compiler, compilation)
-      }
-    )
+    compiler.hooks.beforeRun.tapPromise(PLUGIN_NAME, async compilation => {
+      if (this.filterCompilation(compilation)) return Promise.resolve()
+      debug(`running "beforeRun" hook for compilation: ${compilation.name}`)
+      this.runInitialBuild(compiler, compilation)
+    })
 
     // The "watchRun" hook runs only when using 'watch mode' with webpack, triggering
     // every time that webpack recompiles on a change triggered by the watcher
-    compiler.hooks.watchRun.tapPromise(
-      'PrebuildWebpackPlugin',
-      async compilation => {
-        if (this.filterCompilation(compilation)) return Promise.resolve()
-        debug(`running "watchRun" hook for compilation: ${compilation.name}`)
+    compiler.hooks.watchRun.tapPromise(PLUGIN_NAME, async compilation => {
+      if (this.filterCompilation(compilation)) return Promise.resolve()
+      debug(`running "watchRun" hook for compilation: ${compilation.name}`)
 
-        // By default we cache the matched files for speed. This has the drawback of
-        // potentially having issues with new files that are added. If you're willing
-        // to take the perf hit on every update in exchange for better new file detection
-        // you can enable this option and we'll clear the cache each watch round.
-        // TODO: if we can get webpack to tell us when a new file was added, we could optimize
-        // this such that it only ran in this condition.
-        if (this.clearCacheOnUpdate) {
-          debug('clearing matched files cache')
-          this.matchedFilesCache = null
-        }
-
-        // Since "watchRun" runs every time webpack compiles in watch mode, we limit the
-        // initial build to the first execution.
-        if (this.firstRun) {
-          this.firstRun = false
-          this.runInitialBuild(compiler, compilation)
-        }
-
-        // At this point we're done unless we have a file pattern matcher passed in
-        if (!this.files.pattern) return Promise.resolve()
-
-        // If so, we get the file that was just changed, if there was one changed
-        const changedFile = this.getChangedFile(compiler)
-        if (!changedFile.length) return Promise.resolve()
-
-        // If the changed file doesn't match the provided pattern, we're done
-        const changedMatch = minimatch.match(changedFile, this.files.pattern)
-        if (!changedMatch.length) return Promise.resolve()
-
-        // If it does, we run the user-provided watch function
-        return this.watch(compiler, compilation, changedMatch)
+      // By default we cache the matched files for speed. This has the drawback of
+      // potentially having issues with new files that are added. If you're willing
+      // to take the perf hit on every update in exchange for better new file detection
+      // you can enable this option and we'll clear the cache each watch round.
+      // TODO: if we can get webpack to tell us when a new file was added, we could optimize
+      // this such that it only ran in this condition.
+      if (this.clearCacheOnUpdate) {
+        debug('clearing matched files cache')
+        this.matchedFilesCache = null
       }
-    )
+
+      // Since "watchRun" runs every time webpack compiles in watch mode, we limit the
+      // initial build to the first execution.
+      if (this.firstRun) {
+        this.firstRun = false
+        this.runInitialBuild(compiler, compilation)
+      }
+
+      // At this point we're done unless we have a file pattern matcher passed in
+      if (!this.files.pattern) return Promise.resolve()
+
+      // If so, we get the file that was just changed, if there was one changed
+      const changedFile = this.getChangedFile(compiler)
+      if (!changedFile.length) return Promise.resolve()
+
+      // If the changed file doesn't match the provided pattern, we're done
+      const changedMatch = minimatch.match(changedFile, this.files.pattern)
+      if (!changedMatch.length) return Promise.resolve()
+
+      // If it does, we run the user-provided watch function
+      return this.watch(compiler, compilation, changedMatch)
+    })
 
     // The "emit" hook runs before webpack is about to write out assets, at the end
     // of the compile process.
-    compiler.hooks.emit.tapPromise(
-      'PrebuildWebpackPlugin',
-      async compilation => {
-        if (this.filterCompilation(compilation)) return Promise.resolve()
-        debug(`running "emit" hook for compilation: ${compilation.name}`)
+    compiler.hooks.emit.tapPromise(PLUGIN_NAME, async compilation => {
+      if (this.filterCompilation(compilation)) return Promise.resolve()
+      debug(`running "emit" hook for compilation: ${compilation.name}`)
 
-        // Grab the matched files so we can add them as dependencies
-        // If there are none, we're done
-        const matchedFiles = await this.getMatchedFiles()
-        if (!matchedFiles.length) return Promise.resolve()
+      // Grab the matched files so we can add them as dependencies
+      // If there are none, we're done
+      const matchedFiles = await this.getMatchedFiles()
+      if (!matchedFiles.length) return Promise.resolve()
 
-        // Add all matched files as dependencies to webpack so that they are
-        if (this.files.addFilesAsDependencies) {
-          matchedFiles.map(f => compilation.fileDependencies.add(f))
-        }
-
-        return Promise.resolve()
+      // Add all matched files as dependencies to webpack so that they are
+      if (this.files.addFilesAsDependencies) {
+        matchedFiles.map(f => compilation.fileDependencies.add(f))
       }
-    )
+
+      return Promise.resolve()
+    })
   }
 }
